e7da82a9de2d8d9c8f40b37dfdf4075682e9c01c
fw867 <fw867@163.com>
2017-12-04 16:41:46 +0800

    support for DSM 5.x and 6.x

--- pkg.sh
+++ pkg.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-echo "清理文件夹"
+BUILDDIR=$(cd `dirname $0`;pwd)
+
+echo "1.清理文件夹"
 rm -rf *.spk
 rm -rf *.tgz
-BUILDDIR=$(cd `dirname $0`;pwd)
-echo $BUILDDIR
-echo "1.生成package.tgz"
 
+echo "2.生成package.tgz"
 cd $BUILDDIR/package
 tar -czf $BUILDDIR/package.tgz *
 
-echo "2.生成$1.SPK"
+echo "3.生成$1.SPK"
 cd $BUILDDIR
 tar -cf $BUILDDIR/$1.spk package.tgz scripts WIZARD_UIFILES CHANGELOG INFO LICENSE PACKAGE_ICON.PNG
